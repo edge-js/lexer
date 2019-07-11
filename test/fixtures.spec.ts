2d1617d81ac69d36f8764bc57c15d0f80408123d
Harminder virk <virk.officials@gmail.com>
2019-07-11 20:07:54 +0530

    test: add fixtures to satisfy syntax spec

---
+++ test/fixtures.spec.ts
@@ -0,0 +1,33 @@
+/*
+ * edge-lexer
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+import * as test from 'japa'
+import { fixtures } from '../fixtures/index'
+import { Tokenizer } from '../src/Tokenizer'
+
+const tags = {
+  if: {
+    seekable: true,
+    block: true,
+  },
+  include: {
+    seekable: true,
+    block: false,
+  },
+}
+
+test.group('fixtures', () => {
+  fixtures.forEach((fixture) => {
+    test(fixture.name, (assert) => {
+      const tokenizer = new Tokenizer(fixture.in, tags, { filename: 'mem' })
+      tokenizer.parse()
+      assert.deepEqual(tokenizer.tokens, fixture.out)
+    })
+  })
+})
